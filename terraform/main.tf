c30f8fee9d4888f79c2f7b73f2bb628e6b367903
LyubenGeorgiev <koko6kata1@gmail.com>
2024-01-08 19:39:45 +0200

    fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,17 +8,17 @@ module "eks" {
   endpoint_public_access  = true
   endpoint_private_access = false
   public_access_cidrs     = ["0.0.0.0/0"]
-  node_group_name         = "cloudquicklabs"
+  node_group_name         = "shahNodes"
   scaling_desired_size    = 1
   scaling_max_size        = 1
   scaling_min_size        = 1
-  instance_types          = ["t3.small"]
+  instance_types          = ["t3.micro"]
   key_pair                = "TestKeyPair"
 }
 
 module "vpc" {
   source                  = "./modules/vpc"
-  tags                    = "cloudquicklabs"
+  tags                    = "shah"
   instance_tenancy        = "default"
   vpc_cidr                = "10.0.0.0/16"
   access_ip               = "0.0.0.0/0"
@@ -26,5 +26,4 @@ module "vpc" {
   public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]
   map_public_ip_on_launch = true
   rt_route_cidr_block     = "0.0.0.0/0"
-
 }
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,6 +1,6 @@
-resource "aws_eks_cluster" "cloudquicklabs" {
+resource "aws_eks_cluster" "shah" {
   name     = var.cluster_name
-  role_arn = aws_iam_role.cloudquicklabs.arn
+  role_arn = aws_iam_role.shah.arn
 
   vpc_config {
     subnet_ids              = var.aws_public_subnet
@@ -11,15 +11,15 @@ resource "aws_eks_cluster" "cloudquicklabs" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.cloudquicklabs-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.cloudquicklabs-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.shah-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.shah-AmazonEKSVPCResourceController,
   ]
 }
 
-resource "aws_eks_node_group" "cloudquicklabs" {
-  cluster_name    = aws_eks_cluster.cloudquicklabs.name
+resource "aws_eks_node_group" "shah" {
+  cluster_name    = aws_eks_cluster.shah.name
   node_group_name = var.node_group_name
-  node_role_arn   = aws_iam_role.cloudquicklabs2.arn
+  node_role_arn   = aws_iam_role.shah_nodegroup.arn
   subnet_ids      = var.aws_public_subnet
   instance_types  = var.instance_types
 
@@ -35,9 +35,9 @@ resource "aws_eks_node_group" "cloudquicklabs" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.cloudquicklabs-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.cloudquicklabs-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.cloudquicklabs-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.shah-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.shah-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.shah-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
 
@@ -60,63 +60,65 @@ resource "aws_security_group" "node_group_one" {
   }
 }
 
-resource "aws_iam_role" "cloudquicklabs" {
-  name = "eks-cluster-cloudquicklabs"
+data "aws_iam_policy_document" "assume_role_eks" {
+  statement {
+    effect = "Allow"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
     }
-  ]
-}
-POLICY
+
+    actions = ["sts:AssumeRole"]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "cloudquicklabs-AmazonEKSClusterPolicy" {
+resource "aws_iam_role" "shah" {
+  name               = "eks-cluster-shah"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_eks.json
+}
+
+resource "aws_iam_role_policy_attachment" "shah-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cloudquicklabs.name
+  role       = aws_iam_role.shah.name
 }
 
 # Optionally, enable Security Groups for Pods
 # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "cloudquicklabs-AmazonEKSVPCResourceController" {
+resource "aws_iam_role_policy_attachment" "shah-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.cloudquicklabs.name
+  role       = aws_iam_role.shah.name
 }
 
-resource "aws_iam_role" "cloudquicklabs2" {
-  name = "eks-node-group-cloudquicklabs"
+data "aws_iam_policy_document" "assume_role_ec2" {
+  statement {
+    effect = "Allow"
 
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "cloudquicklabs-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role" "shah_nodegroup" {
+  name = "eks-node-group-shah"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_ec2.json
+}
+
+resource "aws_iam_role_policy_attachment" "shah-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.cloudquicklabs2.name
+  role       = aws_iam_role.shah_nodegroup.name
 }
 
-resource "aws_iam_role_policy_attachment" "cloudquicklabs-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "shah-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.cloudquicklabs2.name
+  role       = aws_iam_role.shah_nodegroup.name
 }
 
-resource "aws_iam_role_policy_attachment" "cloudquicklabs-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "shah-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.cloudquicklabs2.name
+  role       = aws_iam_role.shah_nodegroup.name
 }
